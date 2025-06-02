ed4e414f4f06bb5f640d643cebd4a6e693e927b1
0sifat <ssifat207@gmai.com>
2025-06-02 17:56:13 +0600

    add virustotal

--- Brac/scp.sh
+++ Brac/scp.sh
@@ -25,3 +25,33 @@ echo "Accessing Tomcat server ${TOMCAT_SERVER}..."
 ssh -i $KEY_PATH -p 222 centos@$TOMCAT_SERVER <<EOF
     scp -i $KEY_PATH -P 222 /bits/sbicloud/MS-tomcat9090/webapps/ROOT.war centos@$DST_SERVER:/home/centos/
 EOF
+
+
+
+scp -i /home/centos/.ssh/id_rsa -P 222 /bits/sbicloud/MS-tomcat9090/webapps/ROOT.war centos@52.66.13.94:/home/centos/
+
+
+
+#!/bin/bash
+
+# Variables
+KEY_PATH="/home/centos/.ssh/id_rsa"
+SRC_SERVER="13.126.173.220"
+DST_SERVER="52.66.13.94"
+TOMCAT_SERVER="15.206.49.232"
+WAR_FILE_PATH="/production-data/backup/dbbackup/master/db/"
+DB_BACKUP_PATH="/home/centos/"
+
+scp -i $KEY_PATH -P 222 "$WAR_FILE_PATH/$FILENAME" centos@$DST_SERVER:/home/centos/
+
+
+#!/bin/bash
+
+# Variables
+KEY_PATH="/home/centos/.ssh/id_rsa"
+DST_SERVER="52.66.13.94"
+TOMCAT_SERVER="15.206.49.232"
+WAR_FILE_PATH="/bits/sbicloud/MS-tomcat9090/webapps/ROOT.war"
+DB_BACKUP_PATH="/home/centos/"
+
+scp -i $KEY_PATH -P 222 "$WAR_FILE_PATH/$FILENAME" centos@$DST_SERVER:/home/centos
